fb0404bbf54e4e25b3ed76282c910cb8758b0696
Tylor Sherman <tylor@denimandsteel.com>
2012-10-22 16:32:26 -0700

    As you set out on your journey to Ithaca, pray that your journey be a long one, filled with adventure, filled with discovery.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssetManager::Application.config.secret_token = '3cb6599e9d2c4669a83cb1f583bee13421f7a55afd3aeb83dac11664e624a03f6590391f53217322ef05e7b8c4365fa5f43bee0c706cd0d285c11417d1ae7d1b'

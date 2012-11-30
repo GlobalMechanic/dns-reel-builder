54dcbd9fd3a18051284316d6fcd8011012fcc163
Tylor Sherman <tylor@denimandsteel.com>
2012-11-29 17:15:09 -0800

    Hide add link when it just won't work

--- app/assets/javascripts/clips.js
+++ app/assets/javascripts/clips.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
-  var reelID = $('.reel-id').attr('id').replace(/reel-/, '');
-  $('.clip .reel').click(function() {
-    var $clip = $(this);
-    var clipID = $clip.attr('id').replace(/clip-/, '');
-    $clip.toggleClass('add').toggleClass('remove').addClass('loading');
-    if ($clip.hasClass('add')) {
-      $.get('/reels/' + reelID + '/remove.json', { clip_id: clipID }, function(data) {
-        console.log(data);
-        $clip.removeClass('loading');
-      });
-    }
-    else if ($clip.hasClass('remove')) {
-      $.get('/reels/' + reelID + '/add.json', { clip_id: clipID }, function(data) {
-        console.log(data);
-        $clip.removeClass('loading');
-      });
-    }
-    return false;
-  })
+  $reel = $('.reel-id');
+  if ($reel.length > 0) {
+    var reelID = $reel.attr('id').replace(/reel-/, '');
+    $('.clip .reel').click(function() {
+      var $clip = $(this);
+      var clipID = $clip.attr('id').replace(/clip-/, '');
+      $clip.toggleClass('add').toggleClass('remove').addClass('loading');
+      if ($clip.hasClass('add')) {
+        $.get('/reels/' + reelID + '/remove.json', { clip_id: clipID }, function(data) {
+          console.log(data);
+          $clip.removeClass('loading');
+        });
+      }
+      else if ($clip.hasClass('remove')) {
+        $.get('/reels/' + reelID + '/add.json', { clip_id: clipID }, function(data) {
+          console.log(data);
+          $clip.removeClass('loading');
+        });
+      }
+      return false;
+    });
+  }
+  else {
+    $('.clip .reel').remove();
+  }
 });

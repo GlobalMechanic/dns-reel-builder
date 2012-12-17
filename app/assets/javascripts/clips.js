c93de1ac4aa0616cac9334d859f3f84b4b1501a7
Tylor Sherman <tylor@denimandsteel.com>
2012-12-17 10:21:15 -0800

    No dragging on most pages

--- app/assets/javascripts/clips.js
+++ app/assets/javascripts/clips.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-  if (gm.current_reel_slug === null) {
-    // Create a blank reel.
-  }
-
   // Clip add/remove click events.
   $('.clip .reel').click(function(e) {
     var $clip = $(this);
@@ -28,52 +24,52 @@ $(document).ready(function() {
     return false;
   });
 
-  // Todo: these should only happen on filter pages:
-
-  // Clip drag events.
-  var $dragged = null;
-  $('.clip').bind('dragstart', function(e) {
-    $('.clips').addClass('dragging');
-    $dragged = $(this).addClass('dragged');
-    return true;
-  }).bind('dragend', function(e) {
-    $('.clips').removeClass('dragging');
-    $dragged.removeClass('dragged');
-    return true;
-  });
-
-  // Slot dragover events.
-  $('.slot').bind('dragover', function(e) {
-    $(this).addClass('active');
-    // Drag into a neighbouring slot (no change).
-    if ($dragged.next().get(0) === this || $dragged.prev().get(0) === this) {
+  if (gm.controller === 'reels' && gm.action === 'edit') {
+    // Clip drag events.
+    var $dragged = null;
+    $('.clip').bind('dragstart', function(e) {
+      $('.clips').addClass('dragging');
+      $dragged = $(this).addClass('dragged');
       return true;
-    }
-    else {
+    }).bind('dragend', function(e) {
+      $('.clips').removeClass('dragging');
+      $dragged.removeClass('dragged');
+      return true;
+    });
+
+    // Slot dragover events.
+    $('.slot').bind('dragover', function(e) {
+      $(this).addClass('active');
+      // Drag into a neighbouring slot (no change).
+      if ($dragged.next().get(0) === this || $dragged.prev().get(0) === this) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }).bind('dragleave', function(e) {
+      $(this).removeClass('active');
       return false;
-    }
-  }).bind('dragleave', function(e) {
-    $(this).removeClass('active');
-    return false;
-  }).bind('drop', function(e) {
-    var $this = $(this);
-    var $bringMeAlong = $dragged.prev();
+    }).bind('drop', function(e) {
+      var $this = $(this);
+      var $bringMeAlong = $dragged.prev();
 
-    // Move clip into new slot
-    $dragged.insertAfter($this);
-    $bringMeAlong.insertAfter($dragged);
+      // Move clip into new slot
+      $dragged.insertAfter($this);
+      $bringMeAlong.insertAfter($dragged);
 
-    // Remove active from slot.
-    $this.removeClass('active');
+      // Remove active from slot.
+      $this.removeClass('active');
 
-    // Save new order to server.
-    var clips = [];
-    $('.clip .reel').each(function(index, clip) {
-      clips.push($(clip).attr('id').replace(/clip-/, ''));
+      // Save new order to server.
+      var clips = [];
+      $('.clip .reel').each(function(index, clip) {
+        clips.push($(clip).attr('id').replace(/clip-/, ''));
+      });
+      $.post('/reels/' + gm.current_reel_slug + '/sort.json', { order: clips }, function(data) {
+        // We cool.
+      });
     });
-    $.post('/reels/' + gm.current_reel_slug + '/sort.json', { order: clips }, function(data) {
-      // We cool.
-    });
-  });
+  }
 
 });

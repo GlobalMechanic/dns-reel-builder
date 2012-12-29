e8e99c4944ed9d116b93f41248a1f38201bd2fcd
Tylor Sherman <tylor@denimandsteel.com>
2012-12-28 18:56:55 -0800

    Hide public css and js from backend

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require tabs
-//= require_tree .
+//= require clips
+//= require reel
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
 User.create! :name => 'Admin', :email => 'tylor@denimandsteel.com', :password => 'password', :password_confirmation => 'password'

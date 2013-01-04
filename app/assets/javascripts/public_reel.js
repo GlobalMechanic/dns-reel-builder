1b28d2f144102a254dee099cb2dc2185541f85db
Tylor Sherman <tylor@denimandsteel.com>
2013-01-03 17:25:38 -0800

    Filling out more tray information

--- app/assets/javascripts/public_reel.js
+++ app/assets/javascripts/public_reel.js
@@ -5,18 +5,17 @@
 
 $(document).ready(function() {
   var tray = _.template(
-    ['<div id="tray-613" class="video tray">',
+    ['<div id="tray-<%= clip.id %>" class="video tray">',
       '<div class="inside">',
         '<video class="video-js vjs-default-skin" controls="controls" autoplay="autoplay" data-setup="{}" width="960" height="540">',
-          '<source src="http://globalmechanic.com/clip/movies/<%= video %>" type="video/mp4">',
+          '<source src="http://globalmechanic.com/clip/movies/<%= clip.video %>" type="video/mp4">',
         '</video>',
-        '<!-- controls="controls" autoplay="autoplay"  -->',
         '<div class="meta">',
           '<div class="info">',
             '<ul>',
-              '<li><strong>Director</strong> <%= director %></li>',
-              '<li><strong>Title</strong> <%= title %></li>',
-              '<li><strong>Client</strong> <%= client %></li>',
+              '<li><strong>Director</strong> <%= clip.director %></li>',
+              '<li><strong>Title</strong> <%= clip.title %></li>',
+              '<li><strong>Client</strong> <%= clip.client %></li>',
             '</ul>',
           '</div>',
           '<div class="actions">',
@@ -28,16 +27,16 @@ $(document).ready(function() {
           '<div id="share" class="clearfix">',
             '<div class="column">',
               '<label for="share-url">URL for this clip:</label>',
-              '<input id="share-url" type="text" value="http://globalmechanic.com/reel/1204/613" /><span class="clippy"></span>',
+              '<input id="share-url" type="text" value="<%= root_url %>reels/<%= reel.slug %>" /><span class="clippy"></span>',
             '</div>',
             '<div class="column">',
               '<label>Share it with:</label>',
-              '<a class="twitter" target="_blank" href="http://twitter.com/share?url=http%3A%2F%2Fglobalmechanic.com%2Freel%2F1204%2F613&amp;text=Clip%20from%20Global%20Mechanic%3A%20Montage%203D">Twitter</a>',
+              '<a class="twitter" target="_blank" href="http://twitter.com/share?url=<%= root_url %>reels/<%= reel.slug %>&amp;text=Reel%20from%20Global%20Mechanic%3A%20<%= reel.title %>">Twitter</a>',
               '<a class="email" href="mailto:?subject=Clip%20from%20Global%20Mechanic%3A%20Montage%203D&amp;body=http%3A%2F%2Fglobalmechanic.com%2Freel%2F1204%2F613">Email</a>',
               '<a class="facebook" target="_blank" href="http://www.facebook.com/sharer.php?u=http%3A%2F%2Fglobalmechanic.com%2Freel%2F1204%2F613&amp;t=Clip%20from%20Global%20Mechanic%3A%20Montage%203D">Facebook</a>',
             '</div>',
           '</div>',
-          '<div class="description"><%= description %></div>',
+          '<div class="description"><%= clip.description %></div>',
         '</div>',
       '</div>',
     '</div>'].join('')
@@ -50,7 +49,7 @@ $(document).ready(function() {
     if (!active) {
       $(this).addClass('active');
       var clipID = parseInt($(this).attr('id').replace('clip-', ''));
-      $(this).parent('.clips').after(tray(gm.clips[clipID]));
+      $(this).parent('.clips').after(tray({ root_url: gm.root_url, clip: gm.clips[clipID], reel: gm.reel }));
     }
     return false;
   });

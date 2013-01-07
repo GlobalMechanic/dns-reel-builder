78d70a246656a3ca85b929dcae990548886d15a2
Tylor Sherman <tylor@denimandsteel.com>
2013-01-06 18:14:16 -0800

    Make popovers a little more obvious

--- app/assets/javascripts/reel.js
+++ app/assets/javascripts/reel.js
@@ -4,5 +4,8 @@ $(document).ready(function() {
     $(this).parent().toggleClass('open');
     return false;
   });
+  $('#create-reel').click(function () {
+    $('#reel_title').focus();
+  });
 
 });

18bb3bbb491aabaaba4868e8e7aaf5fb62f66db2
Tylor Sherman <tylor@denimandsteel.com>
2013-01-08 15:57:45 -0800

    Restructure JS a little bit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,38 @@
 //= require zero-clipboard
 //= require clips
 //= require reel
+
+$(document).ready(function() {
+
+  // Dynamic search form.
+  var nameToId = {
+    'all': '#search_title_or_client_or_description_or_category_or_keywords_taggings_tag_name_contains',
+    'title': '#search_title_contains',
+    'director': '#search_director_equals',
+    'category': '#search_category_equals',
+    'client': '#search_client_equals',
+    'technique': '#search_techniques_taggings_tag_name_contains',
+    'keyword': '#search_keywords_taggings_tag_name_contains'
+  };
+
+  // Make 'All' field active if no search criteria.
+  if ($(nameToId['all']).hasClass('show') && $(nameToId['all']).val() == '') {
+    $(nameToId['all']).focus();
+  }
+
+  // Show and hide search facets.
+  $('#clip_search #where').change(function(e) {
+    $('#clip_search .facet').removeClass('show');
+    $(nameToId[$(this).val()]).addClass('show').focus();
+  });
+
+  $('#clip_search select.facet').change(function(e) {
+    $('#clip_search').submit();
+  });
+
+  // Remove inactive search facets before submitting.
+  $('#clip_search').submit(function() {
+    $('#clip_search .facet').not(nameToId[$('#clip_search #where').val()]).remove();
+  });
+
+});
--- app/assets/javascripts/reel.js
+++ app/assets/javascripts/reel.js
@@ -8,4 +8,53 @@ $(document).ready(function() {
     $('#reel_title').focus();
   });
 
+  // Dragging and dropping for reels.
+  if (gm.controller === 'reels' && gm.action === 'edit') {
+    // Clip drag events.
+    var $dragged = null;
+    $('.clip').bind('dragstart', function(e) {
+      $('.clips').addClass('dragging');
+      $dragged = $(this).addClass('dragged');
+      return true;
+    }).bind('dragend', function(e) {
+      $('.clips').removeClass('dragging');
+      $dragged.removeClass('dragged');
+      return true;
+    });
+
+    // Slot dragover events.
+    $('.slot').bind('dragover', function(e) {
+      $(this).addClass('active');
+      // Drag into a neighbouring slot (no change).
+      if ($dragged.next().get(0) === this || $dragged.prev().get(0) === this) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }).bind('dragleave', function(e) {
+      $(this).removeClass('active');
+      return false;
+    }).bind('drop', function(e) {
+      var $this = $(this);
+      var $bringMeAlong = $dragged.prev();
+
+      // Move clip into new slot
+      $dragged.insertAfter($this);
+      $bringMeAlong.insertAfter($dragged);
+
+      // Remove active from slot.
+      $this.removeClass('active');
+
+      // Save new order to server.
+      var clips = [];
+      $('.clip .reel').each(function(index, clip) {
+        clips.push($(clip).attr('id').replace(/clip-/, ''));
+      });
+      $.post('/reels/' + gm.current_reel_slug + '/sort.json', { order: clips }, function(data) {
+        // We cool.
+      });
+    });
+  }
+
 });

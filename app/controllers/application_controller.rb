ac7259dccbc9253116535f4fcb060140aac3e0af
Tylor Sherman <tylor@denimandsteel.com>
2013-01-16 15:08:06 -0800

    Restrict recent to current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   private
   def nav_variables
     @current_reel = current_user && current_user.current_reel_slug? && Reel.exists?(current_user.current_reel_slug) ? Reel.find(current_user.current_reel_slug) : nil
-    @reels_created = Reel.where("title <> ''").order('created_at DESC').limit(5)
-    @reels_updated = Reel.where("title <> ''").order('updated_at DESC').limit(5)
+    @reels_created = Reel.where("title <> ''").where("user_id = ?", current_user.id).order('created_at DESC').limit(5)
+    @reels_updated = Reel.where("title <> ''").where("user_id = ?", current_user.id).order('updated_at DESC').limit(5)
   end
 end

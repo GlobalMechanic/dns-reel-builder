8132770f13e0c6a9383ac38441e60067526d3fd1
Tylor Sherman <tylor@denimandsteel.com>
2013-01-15 17:45:07 -0800

    Tokenize urls in descriptions

--- app/helpers/clips_helper.rb
+++ app/helpers/clips_helper.rb
@@ -1,2 +1,9 @@
 module ClipsHelper
+  def url_token(text)
+    text.gsub(/\[([^\[]*)[\|]([^\]]*)\]/) {
+      text = $1
+      href = $2
+      '<a href="' + (href.match('http://') == nil ? 'http://' : '') + href + '">' + text + '</a>'
+    }
+  end
 end

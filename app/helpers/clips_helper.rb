b023e26e336a560cd4d12acfefa663f918cd3785
Tylor Sherman <tylor@denimandsteel.com>
2013-01-23 22:14:15 -0800

    Error catch on nil text

--- app/helpers/clips_helper.rb
+++ app/helpers/clips_helper.rb
@@ -1,9 +1,13 @@
 module ClipsHelper
   def url_token(text)
-    text.gsub(/\[([^\[]*)[\|]([^\]]*)\]/) {
-      text = $1
-      href = $2
-      '<a href="' + (href.match('http://') == nil ? 'http://' : '') + href + '">' + text + '</a>'
-    }
+    if text
+      text.gsub(/\[([^\[]*)[\|]([^\]]*)\]/) {
+        text = $1
+        href = $2
+        '<a href="' + (href.match('http://') == nil ? 'http://' : '') + href + '">' + text + '</a>'
+      }
+    else
+      text
+    end
   end
 end
